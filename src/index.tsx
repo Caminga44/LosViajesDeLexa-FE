6f2474e31e6d3ebed25f326b0aa236e3d89a2301
Camila Ingaramo <camilaingaramo44@gmail.com>
2025-04-09 20:50:19 +0200

    Creación de la Toolbar

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import {createRoot} from 'react-dom/client';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
 import Home from './Home';
+import Toolbar from './commons/Toolbar';
 
 createRoot (document.getElementById('root')!).render(
   <>
    <BrowserRouter>
+   <Toolbar>
     <Routes>
      <Route path="/" element={<Home /> } />
     </Routes>
+    </Toolbar>
    </BrowserRouter>
   </>
 );

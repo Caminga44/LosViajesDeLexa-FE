318a2b8d1ef3c1ddab8f23c66d983bac347bd337
Camila Ingaramo <camilaingaramo44@gmail.com>
2025-04-22 00:46:55 +0200

    Creación de comentarios

---
+++ src/stores/comentarios/ComentarioStore.tsx
@@ -0,0 +1,28 @@
+import React, {useReducer} from 'react';
+import {IComentarioAction, IComentarios} from './ComentarioIData';
+
+const initialState: IComentarios= {
+    comentarios: []
+}
+
+export const ComentarioStore = React.createContext<IComentarios | any>(initialState);
+
+function comentarioReducer(comentarioState: IComentarios, comentarioAction: IComentarioAction): IComentarios{
+    switch(comentarioAction.type){
+        case 'GET': {
+            return {...comentarioState, comentarios: comentarioAction.payload}
+        }
+        case 'POST': {
+            return {...comentarioState, comentarios: comentarioAction.payload}
+        }
+        case 'PUT': {
+            return {...comentarioState, comentarios: comentarioAction.payload}
+        }
+        default: return comentarioState
+    }
+}
+
+export function ComentarioStoreProvider(props: any){
+    const[comentarioState, dispatch] = useReducer(comentarioReducer, initialState)
+    return(<ComentarioStore.Provider value= {{comentarioState, dispatch}}>{props.children}</ComentarioStore.Provider>)
+}

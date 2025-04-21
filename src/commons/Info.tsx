fe63246dd4902dedff46f21ef7599a5742290898
Camila Ingaramo <camilaingaramo44@gmail.com>
2025-04-21 21:52:58 +0200

    Cambio de "error" a "info"

---
+++ src/commons/Info.tsx
@@ -0,0 +1,31 @@
+import React  from "react";
+import './Error.css'
+
+interface InfoProps {
+    infoState: boolean;
+    info: string;
+    show: (value: boolean) => void;
+    action?: () => void;
+}
+
+const Info: React.FC<InfoProps> = ({ infoState, info, show, action }) => {
+    return (
+        <>
+            {infoState &&
+                <div className="overlay">
+                    <div className="error-container">
+                        <button className= 'error-close' onClick={(e) => {
+                            e.preventDefault()
+                            show(false)}}>X</button>
+                        <h2 className="error-text">{info}</h2>
+                        <button className="error-confirm" onClick={(e) => {
+                            e.preventDefault()
+                            show(false)}}>OK</button>
+                    </div>
+                </div>
+            }
+        </>
+    )
+}
+
+export default Info;
